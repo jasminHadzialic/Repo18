aeb7b1ef5125bf167735269a5087c51e435d13c0
jasminHadzialic <hadzialic.jasmin@gmail.com>
2021-09-30 13:49:20 +0200

    mapping and filtering

---
+++ ex08/src/index.js
@@ -0,0 +1,15 @@
+let one = [...new Set([1, 2, 3, 4, 5])];
+let two = [...new Set([6, 4, 3, 2])];
+
+let unionSet = new Set([...one, ...two]);
+
+let intersectionSet = new Set([...one].filter(x => two.includes(x)));
+let differenceSet = new Set([...one].filter(x => !two.includes(x)));
+
+console.log(one);
+console.log(two);
+console.log(unionSet);
+console.log(intersectionSet);
+console.log(differenceSet);
+
+module.exports = { unionSet, intersectionSet, differenceSet };
